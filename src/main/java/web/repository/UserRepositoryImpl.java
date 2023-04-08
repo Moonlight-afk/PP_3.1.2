3a92c380af719bb60ffc46dca87dcaa62e2aa587
Moonlight-afk <d.potakuev@mail.ru>
2023-04-08 20:19:57 +0300

    Commit

--- src/main/java/web/repository/UserRepositoryImpl.java
+++ src/main/java/web/repository/UserRepositoryImpl.java
@@ -1,11 +1,11 @@
 package web.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.models.Users;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
